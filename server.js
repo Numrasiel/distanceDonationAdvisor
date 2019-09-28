ac247c46c53e8b2478128f4617292cf454b64105
Jesus <gsusss2004@gmail.com>
2019-09-28 15:39:15 +0100

    distance as parammerteer

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
-    const maxRatio=0.005;
-    const minRatio=0.002;
-    const carbFootpintRatio=0.3023614509094777;
-app.get('/:distance', function (req, res) {
-    let distance=req.params.distance;
+const maxRatio = 0.005;
+const minRatio = 0.002;
+const carbFootpintRatio = 0.3023614509094777;
+
+app.get('/', function (req, res) {
+    //let distance = req.params.distance;
+    let distance = req.query.distance;
     //console.log(distance);
-    res.send({"currency":"USD",
-    "low":minRatio*distance, 
-    "hight":maxRatio*distance,
-    "cobonFootprintKg":carbFootpintRatio*distance,
-    "distance":distance});
+    res.send({
+        "currency": "USD",
+        "low": minRatio * distance,
+        "hight": maxRatio * distance,
+        "cobonFootprintKg": carbFootpintRatio * distance,
+        "distance": distance
+    });
 });
 
 app.listen(port);
